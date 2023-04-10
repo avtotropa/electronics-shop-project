130dab0cf848cbac3142e6ba178083b437157827
autotropa <autotropa@yandex.ru>
2023-04-10 18:56:55 +0300

    add tests

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,1 +1,14 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
+
+"""Здесь надо написать тесты с использованием pytest для модуля item."""
+from src.item import Item
+
+
+def test_item(test_class_item):
+    """Тестирование создания объекта Item."""
+    assert isinstance(test_class_item, Item)
+    assert test_class_item.name == "Смартфон"
+    assert test_class_item.price == 10000
+    assert test_class_item.quantity == 20
+    assert test_class_item.calculate_total_price() == 200000
+    assert test_class_item.apply_discount() is None
