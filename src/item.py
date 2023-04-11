b2c3978a29c9d0a5691201e94abd31d5b7b1b562
autotropa <autotropa@yandex.ru>
2023-04-11 20:19:21 +0300

    Переделанный код

--- src/item.py
+++ src/item.py
@@ -8,7 +8,6 @@ class Item:
     def __init__(self, name: str, price: float, quantity: int) -> None:
         """
         Создание экземпляра класса item.
-
         :param name: Название товара.
         :param price: Цена за единицу товара.
         :param quantity: Количество товара в магазине.
@@ -21,13 +20,14 @@ class Item:
     def calculate_total_price(self) -> float:
         """
         Рассчитывает общую стоимость конкретного товара в магазине.
-
         :return: Общая стоимость товара.
         """
-        return self.price * self.quantity
+        return self.price * self.quantity * Item.pay_rate
 
-    def apply_discount(self) -> None:
+    def apply_discount(self, discount: float) -> None:
         """
         Применяет установленную скидку для конкретного товара.
+        :param discount: Скидка в процентах (например, 0.15 для 15% скидки).
         """
-        self.price *= self.pay_rate
+        self.price = self.price * (1 - discount)
+
--- tests/test_item.py
+++ tests/test_item.py
@@ -1,42 +1,20 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
 
-import pytest
+import unittest
 from src.item import Item
 
-@pytest.fixture
-def test_calculate_total_price():
-    # Тест 1: Количество 1, Цена 10.0
-    item = Item("Тестовый Товар", 10.0, 1)
-    assert item.calculate_total_price() == 10.0
+class ItemTests(unittest.TestCase):
+    def setUp(self):
+        self.item = Item("Смартфон", 10.0, 5)
 
-    # Тест 2: Количество 0, Цена 5.0
-    item = Item("Тестовый Товар", 5.0, 0)
-    assert item.calculate_total_price() == 0.0
+    def test_calculate_total_price(self):
+        total_price = self.item.calculate_total_price()
+        self.assertEqual(total_price, 50.0)
 
-    # Тест 3: Количество 5, Цена 2.5
-    item = Item("Тестовый Товар", 2.5, 5)
-    assert item.calculate_total_price() == 12.5
+    def test_apply_discount(self):
+        self.item.apply_discount(0.15)
+        self.assertEqual(self.item.price, 8.5)
 
-def test_apply_discount():
-    # Тест 1: Коэффициент скидки 0.9
-    item = Item("Тестовый Товар", 10.0, 1)
-    item.pay_rate = 0.9
-    item.apply_discount()
-    assert item.price == 9.0
+if __name__ == '__main__':
+    unittest.main()
 
-    # Тест 2: Коэффициент скидки 0.5
-    item = Item("Тестовый Товар", 20.0, 3)
-    item.pay_rate = 0.5
-    item.apply_discount()
-    assert item.price == 10.0
-
-def test_item_creation():
-    # Тест 1: Проверка имени, цены и количества товара
-    item = Item("Тестовый Товар", 10.0, 3)
-    assert item.name == "Тестовый Товар"
-    assert item.price == 10.0
-    assert item.quantity == 3
-
-    # Тест 2: Проверка добавления товара в список 'all'
-    assert item in Item.all
-    assert len(Item.all) == 1
