e3bd8a42a9c8e3a915c049f3ef6ba39a92e07607
autotropa <autotropa@yandex.ru>
2023-04-12 12:54:39 +0300

    Переделанный код

--- tests/test_item.py
+++ tests/test_item.py
@@ -1,20 +1,29 @@
 """Здесь надо написать тесты с использованием pytest для модуля item."""
-
-import unittest
+import pytest
 from src.item import Item
 
-class ItemTests(unittest.TestCase):
-    def setUp(self):
-        self.item = Item("Смартфон", 10.0, 5)
+def test_calculate_total_price():
+    item1 = Item("товар1", 10, 2)
+    assert round(item1.calculate_total_price(), 2) == 20.0
 
-    def test_calculate_total_price(self):
-        total_price = self.item.calculate_total_price()
-        self.assertEqual(total_price, 50.0)
+    item2 = Item("товар2", 5, 3)
+    assert round(item2.calculate_total_price(), 2) == 15.0
 
-    def test_apply_discount(self):
-        self.item.apply_discount(0.15)
-        self.assertEqual(self.item.price, 8.5)
+    item3 = Item("товар3", 2, 10)
+    assert round(item3.calculate_total_price(), 2) == 20.0
 
-if __name__ == '__main__':
-    unittest.main()
+
+def test_apply_discount():
+    item1 = Item("товар1", 10, 2)
+    item1.apply_discount(0.1)
+    assert round(item1.price, 4) == 9.0
+
+    item2 = Item("товар2", 5, 3)
+    item2.apply_discount(0.25)
+    assert round(item2.price, 4) == 3.75
+
+    item3 = Item("товар3", 2, 10)
+    item3.apply_discount(0.05)
+    assert round(item3.price, 4) == 1.9
+
 
